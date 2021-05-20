ae8e4d5af4fbd5b6f7513062ae742bfc068900f5
TomDonnadieu <tomdonnadieu@gmail.com>
2021-05-20 16:55:55 +0200

    bouton

--- ProectCalendar.ui/src/ProectCalendar/ui/GenerateRFCFileHandler.java
+++ ProectCalendar.ui/src/ProectCalendar/ui/GenerateRFCFileHandler.java
@@ -6,7 +6,17 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 
 public class GenerateRFCFileHandler extends AbstractHandler implements IHandler {
-
+	@Override
+	public boolean isHandled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		// TODO Auto-generated method stub
